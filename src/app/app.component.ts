26516e01d64e3a8028464a66b553b710c91be6d8
MAC <mac@MACs-MacBook-Pro.local>
2017-06-12 17:47:43 +0700

    commit-6: style & styling inline component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      color: blue;
+    }
+  `]
 })
 export class AppComponent {
   title: string = 'My Apps';
